367290834bc3739c96046fde7f11ae955306656b
hex909 <ansheer.ms@gmail.com>
2024-08-01 07:52:12 +0530

    changes

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -10,7 +10,7 @@ import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { Button } from './button';
 
 import { useFormState, useFormStatus } from 'react-dom';
-import { authenticate } from '@/app/lib/action';
+import { authenticate } from '@/app/lib/actions';
 
 export default function LoginForm() {
   const [state, dispatch] = useFormState(authenticate, undefined);
